e0face4eef17d01fdba1a2e02a80d331babd516b
Sergii Liebodkin <s.lebedkin@gmail.com>
2018-05-12 17:20:09 +0300

    some classes was added

---
+++ js/TMLSettings.js
@@ -0,0 +1,6 @@
+class TMLSettings {
+    constructor() {
+        this.firstName = "Sergii";
+        this.secondName = "Liebodkin";
+    }
+}
--- js/tex.js
+++ js/tex.js
@@ -1,10 +1,7 @@
-// created SL-180512
-class TextureML {
-    constructor(val) {
-        this.value = val;
-    }
-}
-
 function loadPage() {
+    var settings0 = new TMLSettings();
+    settings0.firstName = "1234565"
+    console.log(settings0.firstName);
+    console.log(settings0.secondName);
     console.log("loadPage runned");
 }
