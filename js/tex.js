bd010752a841b9320975951d3f644ac57bd3946c
Sergii Liebodkin <s.lebedkin@gmail.com>
2018-05-12 17:26:36 +0300

    experiments

--- js/tex.js
+++ js/tex.js
@@ -1,6 +1,5 @@
 function loadPage() {
     var settings0 = new TMLSettings();
-    settings0.firstName = "1234565"
     console.log(settings0.firstName);
     console.log(settings0.secondName);
     console.log("loadPage runned");
