1ffc234e2e1ab945ed54b2c4cc97daa6e57f608b
Sergii Liebodkin <s.lebedkin@gmail.com>
2018-05-12 17:51:47 +0300

    dynamic css added

--- js/tex.js
+++ js/tex.js
@@ -1,14 +1,26 @@
 function loadPage() {
     // create array of settings
     var settings = [
-        new TMLSettings(), 
-        new TMLSettings(), 
-        new TMLSettings(), 
+        new TMLSettings(),
+        new TMLSettings(),
+        new TMLSettings(),
         new TMLSettings()
     ];
     // iterrate by array
     settings.forEach(v => {
-        console.log(v.firstName);
-        console.log(v.secondName);
+        // get main div element
+        var elem = document.getElementById("main_text");
+
+        // append new elements (first name)
+        var p = document.createElement("p");
+        p.innerText = v.firstName;
+        p. className = "name_style";
+        elem.appendChild(p);
+
+        // append new elements (second name)
+        var p = document.createElement("p");
+        p.innerText = v.secondName;
+        p. className = "name_style";
+        elem.appendChild(p);
     });
 }
